18384eec7010ce49ccf9740223bcb111b4c70315
zenith4957 <cmycho@gmail.com>
2019-04-24 23:38:37 -0500

    finally first app

--- MCpolls/admin.py
+++ MCpolls/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-
 from MCpolls.models import Question, Choice
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
